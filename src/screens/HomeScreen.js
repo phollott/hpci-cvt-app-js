784942714c47da268179424c04a2ca523d5acea1
Piers Hollott <piers.hollott@gmail.com>
2021-03-11 16:15:20 -0800

    experimenting with dark mode

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,17 +5,22 @@ import { ThemeProvider } from 'react-native-elements';
 import { useTheme } from 'react-navigation';
 import { gStyle } from '../constants';
 
+
+import { useColorScheme } from 'react-native-appearance';
+
 // components
 import Touch from '../components/Touch';
 
 const HomeScreen = ({ navigation, screenProps }) => {
   const theme = useTheme();
+  const colorScheme = useColorScheme();
 
 // [pmh] this method of setting English/French is problematic because it does not force a reload,
 // so if it is not applied early, screens are already loaded, and will have their language set
+// [pmh] this method of uapplying dark mode should work with RNE, but is untested
 
   return (
-    <ThemeProvider theme={ gStyle.mytheme }>
+    <ThemeProvider theme={ gStyle.mytheme } useDark={ colorScheme === 'dark' }>
 
       <View style={gStyle.container[theme]}>
         <ScrollView contentContainerStyle={gStyle.contentContainer}>
