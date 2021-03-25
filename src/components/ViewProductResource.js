9a4805815b9de59f050424a8d27179204da8effb
Piers Hollott <piers.hollott@gmail.com>
2021-03-25 16:26:32 -0700

    changes to show Consumer Information with CSS from COVIDPortal

--- src/components/ViewProductResource.js
+++ src/components/ViewProductResource.js
@@ -22,25 +22,32 @@ export default class ViewProductResource extends Component {
 
   componentDidMount() {
     var productResource = this.props.route.params.productResource,
-      url = (global.language === 'en-ca') ? "https://covid-vaccine.canada.ca" : "https://vaccin-covid.canada.ca";
+      url = (global.language === 'en-ca') ? "https://covid-vaccine.canada.ca" : "https://vaccin-covid.canada.ca",
+      cssUrl = url + '/info/GCWeb/css/theme.min.css';
     url += productResource.link;
-    
+
     fetch(url).then((resp)=>{ return resp.text() }).then((text)=>{ 
       var $ = cheerio.load(text),
+        $$ = cheerio.load('<html><head><link href="' + cssUrl + '" rel="stylesheet"/></head></html>'),
         prodResourceBlock = $('main');
+
+      $$('body').append($('main').html());
+      
 //      console.log('page html for [' + url + ']: ' + prodResourceBlock.html())
 
       if (productResource.resourceName === 'Consumer Information') {
         console.log('this is consumer information, so let us slice')
-        var $$ = cheerio.load('<table>' + $('table').html() + '</table>');
+        var $$$ = cheerio.load('<html><head><link href="' + cssUrl + '" rel="stylesheet"/></head></html>');
+        $$$('body').append('<table class="table table-hover table-bordered table-responsive">' 
+          + $('table').html() + '</table><hr/>');
         $('div').has('details.span-8').each((i, detail) => {
-          $$('body').append($(detail).html())
+          $$$('body').append($(detail).html())
         });
       }
 
       this.setState({
-        productResourceHtml: prodResourceBlock.html(),
-        highlyExperimentalLettuceSlice: $$.html()
+        productResourceHtml: $$.html(),
+        highlyExperimentalLettuceSlice: $$$.html()
       });
     }).catch(error => {
       console.log('VPR: could not load url ' + url);
@@ -54,7 +61,6 @@ export default class ViewProductResource extends Component {
           originWhitelist={['*']}
           source={{ html: this.state.productResourceHtml }}
           style={{ marginTop: 0 }}
-          scalesPageToFit={false}
         />
       </View>
     );
