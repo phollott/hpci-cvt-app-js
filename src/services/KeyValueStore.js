66110347ba4df7da51f8539f526db0826d874fb3
Mike Jardine <jardinemr@gmail.com>
2021-05-04 18:45:54 -0300

    added StorageService before implementing bookmarks

---
+++ src/services/KeyValueStore.js
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';  // asynchronous, unencrypted, persistent, key-value storage system for RN
+
+export class KeyValueStore {
+  save(key, value) {
+    return AsyncStorage.setItem(key, value);
+  }
+
+  retrieve(key) {
+    return AsyncStorage.getItem(key);
+  }
+
+  delete(key) {
+    return AsyncStorage.removeItem(key);
+  }
+
+  // keyValuePairs ex: [['key1','value1'],['key2','value2']]
+  saveMulti(keyValuePairs) {
+    return AsyncStorage.multiSet(keyValuePairs);
+  }
+
+  // keys ex: ['key1','key2']
+  // returns key value pairs
+  retrieveMulti(keys) {
+    return AsyncStorage.multiGet(keys);
+  }
+
+  deleteMulti(keys) {
+    return AsyncStorage.multiRemove(keys);
+  }
+
+  deleteAll() {
+    return AsyncStorage.clear();
+  }
+}
