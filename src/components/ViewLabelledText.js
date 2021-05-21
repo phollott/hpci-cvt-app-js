78a4bcdd85a91fe9a4344fba1ef205105f039062
Piers Hollott <piers.hollott@gmail.com>
2021-05-21 16:16:19 -0700

    new component for labelled text

---
+++ src/components/ViewLabelledText.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { t } from 'i18n-js';
+
+export default class ViewLabelledText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (  
+      <View style={{ padding: 3 }}>
+        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{ this.props.text }</Text>
+        <Text style={{ fontSize: 9 }}>{ this.props.label }</Text>
+      </View>
+    );
+  }
+}
